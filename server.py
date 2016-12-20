89f3a44c445c2b54e550f22e7c2701c859dcdd07
Milos Zivlak <zivlakmilos@gmail.com>
2016-12-20 13:30:24 +0100

    Create basic server and project structure

---
+++ server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Development key'        # Change this!
+app.config['DEBUG'] = True                          # Change this!
+
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app, host = '0.0.0.0', port = 5001)
+
