559f1764883330277fcc93f99b5eef76193368ae
Milos Zivlak <zivlakmilos@gmail.com>
2017-01-31 17:43:44 +0100

    Added class for exceptions

---
+++ include/exception.h
@@ -0,0 +1,30 @@
+#ifndef _EXCEPTION_H_
+#define _EXCEPTION_H_
+
+#include <exception>
+#include <string>
+#include <ostream>
+
+class Exception : public std::exception
+{
+public:
+    Exception(const std::string &port, int code = 0);
+    virtual ~Exception(void);
+
+    inline virtual const char *what(void) const throw() { return m_message.c_str(); };
+    inline virtual std::string message(void) const { return m_message; };
+    inline virtual int code(void) const { return m_code; };
+
+private:
+    std::string m_message;
+    int m_code;
+};
+
+inline std::ostream &operator<<(std::ostream &lhs, const Exception &rhs)
+{
+    lhs << "[" << rhs.code() << "]: " << rhs.what() << std::endl;
+    lhs << rhs.what();
+    return lhs;
+}
+
+#endif // _EXCEPTION_H_
